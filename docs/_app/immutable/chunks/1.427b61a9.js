468d29c080040f26c52e072b4e8bcc16b62177d0
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2023-07-27 00:49:16 +0100

    init static site codebase, see desc.
    - this might need some minor tweaks to work on GitHub pages

---
+++ docs/_app/immutable/chunks/1.427b61a9.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.0f6c6a25.js";export{t as component};
---
+++ svelte.config.js
@@ -0,0 +1,42 @@
+// Credits to CaptainCodeman : @https://github.com/CaptainCodeman/svelte-theme-select/blob/master/svelte.config.js
+
+import path from 'node:path'; // for dynamic repo_name [optional]
+import adapter from '@sveltejs/adapter-static';
+import * as dotenv from 'dotenv';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+const { parsed } = dotenv.config({
+    path: '.env'
+});
+
+const github_suffix = ".github.io";
+const github_repo_name = `/${(path.resolve("./").split(path.sep)).pop()}`;
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess({
+		postcss: {
+			plugins: [
+				import("tailwindcss").then(()=>true), 
+				import("autoprefixer").then(()=>true), 
+			]
+		}
+	})], /* <= not required at the moment */
+
+	kit: {
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
+			fallback: null,
+		}),
+		paths: {
+			// ~~~ IMPORTANT UPDATE 2023-04
+			// DEV_NOTE # === 'npx vite build' === PLUS YOU MUST manually remove '{github_repo_name + github_suffix}' computed prefix for './_app/etc.' ; refer ./docs/ lines 8-9 !
+			base: Boolean(parsed || process.env.NODE_ENV_PROD) ? '' /* (github_repo_name + github_suffix) */ : (github_repo_name),
+		},
+	},
+}
+
+export default config
